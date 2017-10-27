a93953766d253d77cebc77c4eccdc524949bfc01
cfen <chris.fenn@guardian.co.uk>
2017-10-27 17:54:47 +0100

    Start draw button functional, style fixes for safari and app

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,8 @@ import {groupBy, sortByKeys, shuffle, compareValues, changeFirstObj } from './li
 
 const dataurl = "https://interactive.guim.co.uk/docsdata-test/1mINILM6lN7p0soJ2eHKEd08bW-0Hw3bpf3nhfng2jrA.json";
 
+const groupAniTime = 1000;
+
 var groupsOriginal = [
 	{
 		"group": "A",
@@ -105,7 +107,6 @@ function init(){
 
 		buildView(newObj);
 
-		//fire this function after compiling html to size iframe correctly
 		window.resize();
 		
 	})	
@@ -113,9 +114,32 @@ function init(){
 
 function buildView(newObj){
 	//populatePots(newObj.pots)
+	shufflePots();
 	fadeInTableBGs();
-	populateTeamFields();	
-	animateDraw(newObj);
+	populateTeamFields();
+
+	document.querySelector('.gv-start-button').addEventListener('click', function(){ animateDraw(newObj) });
+	
+
+	
+}
+
+
+function shufflePots(){
+
+	Array.from(document.querySelectorAll('.gv-pot-div')).forEach((el) => {
+		let childArr = [];
+		Array.from(el.children).forEach((child) => {
+			if(child.tagName == "span" || child.tagName == "SPAN"){
+				childArr.push(child);
+			}
+		})	
+		childArr.map((child, k) => {
+			el.appendChild(el.children[Math.random() * k | 0]);
+		})
+	});
+
+
 }
 
 
@@ -136,7 +160,6 @@ function populateTeamFields(){
 
 function compileHTML(newObj){
 
-
 	let dataIn = newObj;
 
 	Handlebars.registerHelper('html_decoder', function(text) {
@@ -155,8 +178,9 @@ function compileHTML(newObj){
         }
     );
 
-
     var newHTML = content(dataIn);
+
+    
 
     return newHTML;
 
@@ -178,11 +202,28 @@ function formatData(data){
     pots = sortByKeys(pots);
     newObj.pots = pots;
 
+    let buckets = pots;
+    buckets.map((bucket,k) => {
+			bucket.shuffleArr = shuffle(bucket.objArr);
+			if (bucket.sortOn == 1){ 
+				bucket.shuffleArr = (changeFirstObj(bucket.shuffleArr, e => e.Team === 'Russia') )
+				bucket.objArr = (changeFirstObj(bucket.objArr, e => e.Team === 'Russia') )
+				bucket.firstPot = true;
+			};
+
+
+		})
+
+
+    //add a  nother shuffle here
+
     var draw = setDrawData(pots)
     newObj.drawArr = draw;
 	newObj.teams = teams;
+	newObj.buckets = buckets;
 
 	return newObj;
+
 }
 
 
@@ -202,10 +243,10 @@ function setDrawData(pots){
 
 			populateGroups(pot.shuffleArr);
 
-
 		})
 
 	groupsOriginal.sort(compareValues('strengthScore')); 
+
 		groupsOriginal.map((o,k) => {
 			if(k == 0){ o.strengthRating = "strongest"}
 			if(k > 0 && k < 3){ o.strengthRating = "strong"}
@@ -213,6 +254,7 @@ function setDrawData(pots){
 			if(k > 4 && k < 7){ o.strengthRating = "weak"}
 			if(k > 6){ o.strengthRating = "weakest"}
 		});
+
 	groupsOriginal.sort(compareValues('group'));
 
 	return groupsOriginal;	
@@ -229,12 +271,15 @@ function populateGroups(a){
 
 function animateDraw(a){
 	
-	const groupAniTime = 8000;
+
+
+	document.querySelector(".gv-pot-div-intro").classList.add("display-none");
+
+
 
 	Array.from(a.pots).forEach((pot,k) => {
 		
 		var currentPotNum = pot.sortOn
-
 		
 		let nextPotTime = setTimeout(function(){
 
@@ -242,19 +287,34 @@ function animateDraw(a){
 				
 				if(el.getAttribute("potRef") == currentPotNum){
 					el.classList.remove("display-none");
+					document.querySelector(".gv-pot-header").innerHTML = "Seeding pot "+currentPotNum;
+
+
 				}
 
 				if(el.getAttribute("potRef") != currentPotNum){
 					el.classList.add("display-none");
 				}
 			})
+
 			animateTeams(pot.shuffleArr, groupAniTime);
-		}, k * groupAniTime)
-        //console.log(el.getAttribute("data-team"), k);
-        //el.classList.add( el.getAttribute("data-strength"))
+
+		}, k * groupAniTime);
+
     });
 
-	
+
+	let drawDone = setTimeout(function(){
+
+		Array.from(document.querySelectorAll('.gv-group-table')).forEach((table,k) => {
+			 table.classList.add(table.getAttribute("data-strength"));
+		})
+
+		Array.from(document.querySelectorAll('.gv-table-col-head')).forEach((el,k) => {
+			el.innerHTML = el.getAttribute("data-strength");					
+		})	
+
+	}, 4*groupAniTime);
 }
 
 function animateTeams(a,groupAniTime){
@@ -263,185 +323,27 @@ function animateTeams(a,groupAniTime){
 		let currentTeamName = team.Team;
 		let nextTeamTime = setTimeout(function(){
 
-			Array.from(document.querySelectorAll('.gv-row-team')).forEach((el) => {
-				
+			Array.from(document.querySelectorAll('.gv-team-row')).forEach((el) => {
 				if(el.getAttribute("data-team") == currentTeamName){
 					el.classList.remove("display-none");
 				}
-
-				
 			})
 
-		}, k * teamAniTime)
+			Array.from(document.querySelectorAll('.gv-pot-item')).forEach((el) => {
+				if(el.getAttribute("data-team") == currentTeamName){
+					el.classList.add("fifty-pc");
+				}
+			})
+
+		}, k * teamAniTime);
+
+		
+
 	})
+	//fire this function after compiling html to size iframe correctly
+		
 }
 
-
-
-// var simulatorTemplate = Handlebars.compile( 
-//     require('./html/simulator-template.html'), 
-//     { compat: true }
-// );
-
-// function createDraw(first) {
-// 	if(nextPotTime){
-// 		clearTimeout(nextPotTime);
-// 		nextPotTime = null;
-// 	}
-// 	if(nextTeamTime){
-// 		clearTimeout(nextTeamTime);
-// 		nextTeamTime = null;
-// 	}
-// 	groups = JSON.parse(JSON.stringify(groupsOriginal));
-// 	var order = 1;
-// 	document.querySelector('#current-pot p').innerHTML = "";
-// 	if(first){
-// 		document.querySelector('#current-pot p').innerHTML = "<span>Ready to start</span>";
-// 	}
-// 	document.querySelector('#current-pot #current-flags').innerHTML = "";
-// 	document.querySelector('#current-pot').className -= " draw-ended";
-
-// 	pots.forEach(function(pot,potNumber){
-// 		var range = (potNumber === 0) ? [1,2,3,4,5] : [0,1,2,3,4,5];
-
-// 		pot.teams.forEach(function(team, teamNumber){
-// 			var randomNumber = Math.floor(Math.random()*range.length);
-// 			var randomGroup = groups[range[randomNumber]];
-// 			range.splice(randomNumber,1);
-
-// 			team.no = order;
-
-
-// 			if(potNumber === 0){
-// 				randomGroup.teams[0] = team;
-// 			}else{
-// 				var availableSlots = [];
-
-// 				randomGroup.teams.forEach(function(teamSlot,i){
-// 					if(!teamSlot){
-// 						availableSlots.push(i);
-// 					}
-// 				})
-
-// 				var randomPositionNumber = Math.floor(Math.random() * availableSlots.length);
-// 				var randomSlot = availableSlots[randomPositionNumber];
-
-// 				randomGroup.teams[randomSlot] = team;
-// 			}
-
-// 			order++;
-// 		})
-// 	})
-
-	
-
-// 	groups.forEach(function(group){
-// 		var groupDifficulty = 0;
-// 		group.teams.forEach(function(team){
-// 			groupDifficulty += team.points;
-// 		})
-// 		group.difficulty = (groupDifficulty - 14);
-// 	})
-// 	if(first){
-// 		groups = groupsOriginal;
-// 	}
-	
-	
-// 	var simulatorHTML = simulatorTemplate({groups:groups, selectedCountry: selectedCountry});
-// 	document.querySelector('#draw-container').innerHTML = simulatorHTML;
-
-
-// 	animateDraw(first);
-// }
-
-// function animateDraw(a, first){
-// 	var currentPot = 1;
-// 	if(!first){
-// 		updatePot(currentPot);
-// 	}
-// }
-	
-// function animateTeam(no,newPot){
-// 		var teamEl = document.querySelector('.team-order-' + no);
-// 		var currentFlagEl = document.querySelector('.current-flag-' + teamEl.getAttribute('data-team-id'));
-
-// 		teamEl.className += " drawn";
-// 		if(currentFlagEl){
-// 			currentFlagEl.className += " drawn";
-// 		}
-
-// 		if(no < 23){
-// 			var testspeed = false;
-// 			if(no === 5 || no === 11 || no === 17){
-// 				no++;
-// 				currentPot++;
-// 				setTimeout(function(){
-// 					updatePot(currentPot);
-// 				},testspeed || 500)
-// 				nextPotTime = setTimeout(function(){
-// 					animateTeam(no,currentPot);
-// 				},testspeed || 1500)
-// 			}else{
-// 				no++;
-// 				nextTeamTime = setTimeout(function(){
-// 					animateTeam(no,false);
-// 				},testspeed || 200)
-// 			}
-// 		}else if(no === 23){
-// 			onAnimationEnd(groups);
-// 		}
-// 	}
-// 	if(!first){
-// 		animateTeam(0,currentPot);
-// 	}else{
-// 		var teamEls = document.querySelectorAll('.group-container ul li');
-// 		for( var i=0; i<teamEls.length; i++){
-// 			teamEls[i].className += " drawn";
-// 		}
-// 	}
-	
-// }
-
-// function updatePot(pot){
-// 	var statusEl = document.querySelector('#current-pot');
-// 	statusEl.querySelector('p').innerHTML = "Seeding pot " + pot;
-
-// 	var statusFlagsEl = statusEl.querySelector('#current-flags');
-// 	statusFlagsEl.innerHTML = "";
-
-// 	pots[pot-1].teams.forEach(function(team){
-// 		var flagEl = document.createElement('div');
-// 		flagEl.className = "current-flag-container current-flag-" + team.id;
-// 		flagEl.innerHTML = "<img src='" + team.flag + "' />";
-// 		statusFlagsEl.appendChild(flagEl);
-// 	})
-// }
-
-// function onAnimationEnd(){
-// 	var odometerValues = {
-// 		0: 4,
-// 		1: 4,
-// 		2: -11,
-// 		3: -26,
-// 		4: -41,
-// 		5: -56,
-// 		6: -71,
-// 		7: -86,
-// 		8: -101,
-// 		9: -116,
-// 		10: -131
-// 	}
-
-// 	var groupContainers = document.querySelectorAll('.group-container');
-
-// 	for(var i = 0; i<groupContainers.length; i++){
-// 		var diffValue = odometerValues[groups[i].difficulty];
-
-// 		groupContainers[i].querySelector('h3 .odometer').style.backgroundPosition = "0 " + (diffValue) + "px";
-// 	}
-
-// 	document.querySelector('#current-pot').className += " draw-ended";
-// }
 
 
 init();
